bd77b90f6861dca94cec04c12b414ddb0a18981a
Ilham Sidik <ilhamsidikc@gmail.com>
2024-12-19 22:56:06 +0700

    advance update

--- app.js
+++ app.js
@@ -49,8 +49,23 @@ async function connectToWhatsapp() {
             }
         });
 
-        socket.ev.on('messages.upsert', ({ messages }) => {
+        socket.ev.on('messages.upsert', async ({ messages }) => {
             console.log('New message received:', messages);
+            
+
+            if (!messages[0].key.fromMe && messages[0].message.liveLocationMessage && messages[0].key.remoteJid == '6285134564592@s.whatsapp.net') {
+                console.log("=================================");
+                console.log(messages[0].message.liveLocationMessage);
+                console.log("Longitude: " + messages[0].message.liveLocationMessage.degreesLongitude);
+                console.log("Latitude : " + messages[0].message.liveLocationMessage.degreesLatitude);
+                console.log("Sender : " + messages[0].key.remoteJid);
+                console.log("deg Clock Magnetic: " + messages[0].message.liveLocationMessage.degreesClockwiseFromMagneticNorth);
+                console.log("Speed: " + messages[0].message.liveLocationMessage.speedInMps);
+                console.log("Acuracy: " + messages[0].message.liveLocationMessage.accuracyInMeters);
+
+                // Reply the Message
+                await socket.sendMessage(messages[0].key.remoteJid, { text: 'Location Oke' }, { quoted: messages[0] })
+            }
         });
     } catch (error) {
         console.error('Error connecting to WhatsApp:', error);
@@ -113,8 +128,86 @@ app.post('/send-message', async (req, res) => {
     }
 });
 
+// Endpoint untuk mengirim pesan
+app.post('/send-location', async (req, res) => {
+    const { phoneNumber, lon, lat } = req.body;
+
+    if (!phoneNumber || !lon || !lat) {
+        return res.status(400).json({ message: 'Phone number and message are required' });
+    }
+
+    try {
+        const { phoneNumber, lon, lat } = req.body;
+
+        // Tunggu hingga koneksi socket selama 1 menit
+        const isConnected = await waitForConnection();
+
+        if (!isConnected) {
+            return res.status(500).json({ message: 'WhatsApp is not connected after 1 minute' });
+        }
+
+        const jid = `${phoneNumber}@s.whatsapp.net`;
+        await socket.sendMessage(jid, { location: { degreesLatitude: lat, degreesLongitude: lon } });
+        res.status(200).json({ message: 'Message sent successfully' });
+
+    } catch (error) {
+        console.error('Error sending message:', error);
+        res.status(500).json({ message: 'An error occurred while sending the message', error: error.message });
+    }
+});
+
+
+// Endpoint untuk mengirim pesan
+app.post('/send-group-message', async (req, res) => {
+    const { phoneNumber, message } = req.body;
+    
+    console.log(phoneNumber, message);
+
+    if (!phoneNumber || !message) {
+        return res.status(400).json({ message: 'Phone number and message are required' });
+    }
+
+    try {
+        const { phoneNumber, message } = req.body;
+
+        // Tunggu hingga koneksi socket selama 1 menit
+        const isConnected = await waitForConnection();
+
+        if (!isConnected) {
+            return res.status(500).json({ message: 'WhatsApp is not connected after 1 minute' });
+        }
+
+        const jid = `${phoneNumber}@g.us`;
+        await socket.sendMessage(jid, { text: message });
+        res.status(200).json({ message: 'Message sent successfully' });
+
+    } catch (error) {
+        console.error('Error sending message:', error);
+        res.status(500).json({ message: 'An error occurred while sending the message', error: error.message });
+    }
+});
+
 // Memulai server Express
 app.listen(PORT, () => {
     connectToWhatsapp(); // Memastikan WhatsApp selalu terhubung saat server diaktifkan
 });
 // connectToWhatsapp(); // Memastikan WhatsApp selalu terhubung saat server diaktifkan
+
+
+    // New message received: [
+    //     {
+    //     key: {
+    //         remoteJid: '6285134564592@s.whatsapp.net',
+    //         fromMe: false,
+    //         id: '1FF45E2391D195BBE2CDBEFE0A02E5E9',
+    //         participant: undefined
+    //     },
+    //     messageTimestamp: 1734620510,
+    //     pushName: 'Zeki',
+    //     broadcast: false,
+    //     message: Message {
+    //         locationMessage: [LocationMessage],
+    //         messageContextInfo: [MessageContextInfo]
+    //     }
+    //     }
+    // ]
